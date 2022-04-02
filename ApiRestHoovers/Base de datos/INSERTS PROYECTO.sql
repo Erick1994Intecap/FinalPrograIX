94c777814b1e0f407289433b5a948e5f370899c5
Er <erickruiz1994@gmail.com>
2022-04-02 12:35:59 -0600

    Archios de bd

---
+++ ApiRestHoovers/Base de datos/INSERTS PROYECTO.sql
@@ -0,0 +1,115 @@
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Bunni', 'Ackeroyd', '4573816720');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Arley', 'Swetman', '1865437463');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Barn', 'Van der Brugge', '7568720201');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Colin', 'Igo', '5433888213');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Ingeberg', 'Sharer', '5277608383');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Milo', 'Poulney', '5361210940');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Ulrikaumeko', 'Mawdsley', '1932573061');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Meade', 'Wapples', '7175592135');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('York', 'Oakwood', '8511859153');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Will', 'Parvin', '3043982535');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Charmian', 'Mullane', '9507997512');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Fanchette', 'Domino', '1667858464');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Karlik', 'Shuttlewood', '9674760238');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Dorey', 'Vedyaev', '5474599229');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Ralina', 'Rossander', '2572030426');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Pearce', 'Steuart', '6753490226');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Rachele', 'Scadding', '1305537190');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Akim', 'Fernandez', '9021179091');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Sue', 'McCromley', '3458033856');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Xever', 'Odda', '6284360711');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Salome', 'Thow', '2561788121');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Rasla', 'Capoun', '2035392022');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Hedy', 'Stillwell', '9735647726');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Bernadine', 'Grainger', '2166883340');
+insert into CLIENTE (NOMBRE, APELLIDO, TELEFONO) values ('Garreth', 'Quinn', '6174687980');
+
+
+
+INSERT INTO TIPO_VEHICULO(NOMBRE, DESCRIPCION)
+VALUES
+('Todo Terreno','vehiculos 4x4'),
+('Deportivo','Hechos para amantes de la velocidad'),
+('Sedan','4 puertas'),
+('PickUp','Sin cabina'),
+('Hatchback','Vehiculos pequeños')
+
+
+
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2005, 'Chevrolet', 'Express 3500', 4);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2009, 'Volkswagen', 'Routan', 1);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1995, 'Land Rover', 'Defender 90', 2);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2011, 'Rolls-Royce', 'Ghost', 2);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1986, 'Volkswagen', 'Type 2', 4);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1994, 'Ford', 'Lightning', 1);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1993, 'Pontiac', 'Sunbird', 3);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2006, 'Chevrolet', 'Aveo', 2);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1992, 'Honda', 'Prelude', 2);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1993, 'Dodge', 'Spirit', 1);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1994, 'Isuzu', 'Space', 5);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2000, 'Volkswagen', 'GTI', 2);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2008, 'Lotus', 'Exige', 5);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1994, 'Cadillac', 'DeVille', 5);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2002, 'Dodge', 'Ram Van 2500', 5);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2010, 'Porsche', 'Cayenne', 4);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2005, 'Lamborghini', 'Murciélago', 2);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2000, 'GMC', 'Yukon', 5);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2004, 'GMC', 'Envoy XUV', 2);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1995, 'GMC', 'Vandura G1500', 3);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2000, 'Isuzu', 'Trooper', 4);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1989, 'Mitsubishi', 'Eclipse', 1);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1989, 'Lincoln', 'Town Car', 5);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (1985, 'Volkswagen', 'GTI', 1);
+insert into VEHICULO (MODELO, NOMBRE, DESCRIPCION, ID_TIPO) values (2000, 'Jeep', 'Wrangler', 5);
+
+
+
+INSERT INTO departamento(nombre) VALUES ('Alta Verapaz');
+INSERT INTO departamento(nombre) VALUES ('Baja Verapaz');
+INSERT INTO departamento(nombre) VALUES ('Chimaltenango');
+INSERT INTO departamento(nombre) VALUES ('Chiquimula');
+INSERT INTO departamento(nombre) VALUES ('El Progreso');
+INSERT INTO departamento(nombre) VALUES ('Escuintla');
+INSERT INTO departamento(nombre) VALUES ('Guatemala');
+INSERT INTO departamento(nombre) VALUES ('Huehuetenango');
+INSERT INTO departamento(nombre) VALUES ('Izabal');
+INSERT INTO departamento(nombre) VALUES ('Jalapa');
+INSERT INTO departamento(nombre) VALUES ('Jutiapa');
+INSERT INTO departamento(nombre) VALUES ('Petén');
+INSERT INTO departamento(nombre) VALUES ('Quetzaltenango');
+INSERT INTO departamento(nombre) VALUES ('Quiché');
+INSERT INTO departamento(nombre) VALUES ('Retalhuleu');
+INSERT INTO departamento(nombre) VALUES ('Sacatepéquez');
+INSERT INTO departamento(nombre) VALUES ('San Marcos');
+INSERT INTO departamento(nombre) VALUES ('Santa Rosa');
+INSERT INTO departamento(nombre) VALUES ('Solola');
+INSERT INTO departamento(nombre) VALUES ('Suchitepéquez');
+INSERT INTO departamento(nombre) VALUES ('Totonicapán');
+INSERT INTO departamento(nombre) VALUES ('Zacapa');
+
+
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (15, 15, '2022-03-06', 2641.36, 1, 8);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (22, 16, '2022-02-27', 3054.6, 1, 21);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (24, 21, '2022-02-15', 1714.64, 1, 17);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (21, 18, '2022-02-16', 2358.05, 1, 12);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (20, 8, '2022-02-28', 3095.53, 1, 9);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (20, 16, '2022-02-04', 1304.41, 1, 3);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (15, 8, '2022-02-21', 3280.95, 1, 12);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (15, 12, '2022-02-26', 2412.56, 1, 16);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (8, 1, '2022-03-10', 2735.99, 1, 5);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (18, 16, '2022-03-05', 1287.95, 1, 2);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (11, 14, '2022-02-22', 2729.21, 1, 20);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (18, 14, '2022-02-26', 1810.54, 1, 9);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (14, 21, '2022-03-25', 3292.24, 1, 2);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (1, 6, '2022-02-02', 3196.06, 1, 21);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (7, 4, '2022-02-14', 1892.83, 1, 5);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (10, 2, '2022-02-11', 2862.74, 1, 12);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (6, 23, '2022-03-22', 2662.63, 1, 14);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (4, 2, '2022-03-27', 2664.71, 1, 8);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (20, 18, '2022-03-04', 2049.89, 1, 21);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (18, 18, '2022-02-16', 2517.84, 1, 19);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (13, 11, '2022-02-01', 1496.67, 1, 11);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (8, 17, '2022-02-17', 2346.39, 1, 18);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (11, 11, '2022-02-23', 1543.81, 1, 4);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (1, 8, '2022-03-06', 2715.75, 1, 4);
+insert into VIAJE (ID_CLIENTE, ID_VEHICULO, FECHA_VIAJE, PRECIO_VIAJE, VIAJE_REALIZADO, ID_DEPTO_VIAJE) values (11, 5, '2022-03-09', 3462.5, 1, 2);
